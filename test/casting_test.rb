fca742d75b6cec425e43f4d3dc7b23ca34cc1860
Jim Gay <jim@saturnflyer.com>
2013-03-20 06:37:53 -0400

    load test persion in method. remove CoverMe from test

--- test/casting_test.rb
+++ test/casting_test.rb
@@ -29,6 +29,10 @@ class Unrelated
 end
 
 describe Casting::Delegation do
+  def test_person
+    TestPerson.new
+  end
+
   it 'initializes with method name and object' do
     assert Casting::Delegation.new('some_method', Object.new)
   end
@@ -48,7 +52,7 @@ describe Casting::Delegation do
   end
 
   it 'sets an attendant to an object of an ancestor class of the object class' do
-    attendant = TestPerson.new
+    attendant = test_person
     client = SubTestPerson.new
 
     delegation = Casting::Delegation.new('name', client)
@@ -56,23 +60,23 @@ describe Casting::Delegation do
   end
 
   it 'calls a method defined on another object of the same type' do
-    client = TestPerson.new
-    attendant = TestPerson.new
+    client = test_person
+    attendant = test_person
     attendant.extend(TestPerson::Greeter)
     delegation = Casting::Delegation.new('greet', client).to(attendant)
     assert_equal 'hello', delegation.call
   end
 
   it 'passes arguments to a delegated method' do
-    client = TestPerson.new
-    attendant = TestPerson.new
+    client = test_person
+    attendant = test_person
     attendant.extend(TestPerson::Verbose)
     delegation = Casting::Delegation.new('verbose', client).to(attendant).with('arg1','arg2')
     assert_equal 'arg1,arg2', delegation.call
   end
 
   it 'delegates when given a module' do
-    client = TestPerson.new
+    client = test_person
     delegation = Casting::Delegation.new('greet', client).to(TestPerson::Greeter)
     assert_equal 'hello', delegation.call
   end
@@ -87,7 +91,7 @@ describe Casting::Client do
 
     assert_equal attendant.greet, client.delegate('greet', attendant)
   end
-  
+
   it 'passes additional parameters to the attendant' do
     client = TestPerson.new
     client.extend(Casting::Client)
@@ -99,15 +103,13 @@ describe Casting::Client do
 
     assert_equal attendant_output, client_output
   end
-  
+
   it 'passes the object as the client for delegation' do
     client = Object.new
     client.extend(Casting::Client)
 
     delegation = client.delegation('id')
-    
+
     assert_equal client, delegation.client
   end
 end
-
-CoverMe.complete!
