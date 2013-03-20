ea601667a05a7eff79fa2eac1530b3fe92427e29
Jim Gay <jim@saturnflyer.com>
2013-03-20 06:36:50 -0400

    use simplecov for coverage reports

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,5 @@
-require 'cover_me'
-
-CoverMe.config do |c|
-  c.project.root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-  c.file_pattern = [/#{CoverMe.config.project.root}\/lib\/.+\.rb/]
-end
+require 'simplecov'
+SimpleCov.start
 
 require 'minitest/spec'
 require 'minitest/autorun'
