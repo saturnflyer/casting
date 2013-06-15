31a58642fcbc0efdd63a332d8ab1f2167ad9172f
Jim Gay <jim@saturnflyer.com>
2013-06-15 10:35:56 -0400

    add example using delegate vs forward

--- examples/casting_forwardable.rb
+++ examples/casting_forwardable.rb
@@ -1,11 +1,15 @@
 # Provide a feature like the Forwardable library,
 # but this leaves the methods always present.
+# It provides a class level "delegate" method to do
+# delegation, but adds a "forward" method to do non-delegation
+# method forwarding.
 #
 # class SomeClass
 #   include Casting::Client
 #   extend CastingForwardable
 #
 #   delegate [:name, :id] => :collaborator, [:color, :type] => :partner
+#   forward :description => :describer, [:settings, :url] => :config
 # end
 #
 # This will define methods on instances that delegate to the
@@ -41,6 +45,16 @@ module CastingForwardale
       Array(key).each do |prepared_method|
         define_method prepared_method do
           delegate(prepared_method, self.__send__(value))
+        end
+      end
+    end
+  end
+
+  def forward(options)
+    options.each_pair do |key, value|
+      Array(key).each do |prepared_method|
+        define_method prepared_method do
+          self.__send__(value).__send__(key)
         end
       end
     end
