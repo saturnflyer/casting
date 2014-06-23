9d782132919197fb7515695e31115e583915230b
Jim Gay <jim@saturnflyer.com>
2014-06-23 12:35:24 -0400

    skip rebinding in ruby 1.9

--- test/null_module_test.rb
+++ test/null_module_test.rb
@@ -1,21 +1,23 @@
 require 'test_helper'
 
-describe Casting::Null do
-  it 'will answer to any method with nil' do
-    client = TestPerson.new
-    client.extend(Casting::Client)
-    attendant = Casting::Null
+if test_rebinding_methods?
+  describe Casting::Null do
+    it 'will answer to any method with nil' do
+      client = TestPerson.new
+      client.extend(Casting::Client)
+      attendant = Casting::Null
 
-    assert_nil client.delegate('greet', attendant)
+      assert_nil client.delegate('greet', attendant)
+    end
   end
-end
 
-describe Casting::Blank do
-  it 'will answer to any method with an empty string' do
-    client = TestPerson.new
-    client.extend(Casting::Client)
-    attendant = Casting::Blank
+  describe Casting::Blank do
+    it 'will answer to any method with an empty string' do
+      client = TestPerson.new
+      client.extend(Casting::Client)
+      attendant = Casting::Blank
 
-    assert_empty client.delegate('greet', attendant)
+      assert_empty client.delegate('greet', attendant)
+    end
   end
 end
