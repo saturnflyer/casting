cf276f27d04fd2a337033a80d87a940cd63c6fea
Jim Gay <jim@saturnflyer.com>
2015-12-29 14:31:53 -0500

    add a custom initializer and rework context

--- lib/casting/context.rb
+++ lib/casting/context.rb
@@ -21,33 +21,72 @@
 #
 module Casting
   module Context
-    def context
-      self
+
+    def self.extended(base)
+      base.send(:include, InstanceMethods)
     end
-  
-    def assignments
-      @assignments ||= []
+
+    def initialize(*setup_args)
+      attr_reader(*setup_args)
+      private(*setup_args)
+
+      mod = Module.new
+      line = __LINE__; string = %<
+        def initialize(#{setup_args.join(',')})
+          #{setup_args.map do |arg|
+            ['@',arg,' = ',arg].join
+          end.join("\n")}
+          #{setup_args.map do |arg|
+            ["assign(",arg,", self.role_for('",arg,"'))"].join
+          end.join("\n")}
+          Thread.current[:context] = self
+        end
+      >
+      mod.class_eval string, __FILE__, line
+      const_set('Initializer', mod)
+      include mod
     end
-  
-    def assign(*collection)
-      Array(collection).each do |pair|
-        assignments << pair
-      end
-    end
-  
-    def role_for(name)
-      self.class.const_get(name.to_s.capitalize)
-    rescue NameError
-      Object
-    end
-  
-    refine Object do
+
+    module InstanceMethods
       def context
-        Thread.current[:context]
+        self
       end
-      
-      def context=(obj)
-        Thread.current[:context] = obj
+  
+      def assignments
+        @assignments ||= []
+      end
+  
+      def assign(object, role)
+        assignments << [object, role]
+      end
+
+      def dispatch(object, method_name, *args, &block)
+        object.cast(method_name, context.role_implementing(object, method_name), *args, &block)
+      end
+    
+      def role_implementing(object, method_name)
+        assigned_roles(object).find{|role| role.method_defined?(method_name) }
+      end
+    
+      def assigned_roles(object)
+        assignments.select{|pair|
+          pair.first == object
+        }.map(&:last)
+      end
+
+      def role_for(name)
+        role_name = name.to_s.gsub(/(?:^|_)([a-z])/) { $1.upcase }
+        self.class.const_get(role_name)
+      rescue NameError
+        Module
+      end
+    
+      def role_implements?(object, method_name)
+        roles = assigned_roles(object).compact
+        return false if roles.empty?
+        roles.any?{|role|
+          role.method_defined?(method_name)
+        }
       end
     
       def r(name)
@@ -57,21 +96,15 @@ module Casting
       def tell(name, meth)
         r(name).cast(meth, context.role_for(name))
       end
-    
-      def role_implements?(object, method_name)
-        roles = assigned_roles(object).compact
-        return false if roles.empty?
-        assigned_roles(object).compact.any?{|role|
-          role.method_defined?(method_name)
-        }
+    end
+
+    refine Object do
+      def context
+        Thread.current[:context]
       end
-    
-      def dispatch(object, method_name, *args, &block)
-        object.cast(method_name, context.role_implementing(object, method_name), *args, &block)
-      end
-    
-      def role_implementing(object, method_name)
-        assigned_roles(object).find{|role| role.method_defined?(method_name) }
+
+      def context=(obj)
+        Thread.current[:context] = obj
       end
     
       def method_missing(meth, *args, &block)
@@ -82,14 +115,6 @@ module Casting
         else
           super
         end
-      end
-    
-      private
-    
-      def assigned_roles(object)
-        assignments.select{|pair|
-          pair.first == object 
-        }.map(&:last)
       end
     end
   end
