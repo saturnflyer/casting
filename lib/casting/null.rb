db6292ec6edabdfe9880bd06180f63ee8efae204
Jim Gay <jim@saturnflyer.com>
2016-01-12 14:42:31 -0500

    remove references to unused arguments

--- lib/casting/null.rb
+++ lib/casting/null.rb
@@ -1,23 +1,23 @@
 module Casting
   module Null
-    def self.instance_method(name)
+    def self.instance_method(_)
       Empty.instance_method(:null)
     end
-    def self.method_defined?(meth)
+    def self.method_defined?(_)
       true
     end
   end
   module Blank
-    def self.instance_method(name)
+    def self.instance_method(_)
       Empty.instance_method(:blank)
     end
-    def self.method_defined?(meth)
+    def self.method_defined?(_)
       true
     end
   end
   module Empty
-    def null(*args, &block); end
-    def blank(*args, &block)
+    def null(*, &_block); end
+    def blank(*, &_block)
       ""
     end
   end 
