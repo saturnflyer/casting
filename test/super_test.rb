f370465ad70d31718a14519fa89cabf31dc30774
Jim Gay <jim@saturnflyer.com>
2016-07-28 16:46:03 -0400

    test missing methods raise correct error

--- test/super_test.rb
+++ test/super_test.rb
@@ -28,4 +28,16 @@ describe Casting, 'modules using delegate_super' do
 
     assert_equal 'this way or any way and that way!', client.which_way
   end
+
+  it 'raises an error when method is not defined' do
+    client = TestPerson.new.extend(Casting::Client)
+    client.delegate_missing_methods
+    client.cast_as(AnyWay, ThisWay, ThatWay)
+
+    err = expect{
+      client.non_existant_method
+    }.must_raise(NoMethodError)
+
+    expect(err.message).must_match /undefined method \`non_existant_method'/
+  end
 end
